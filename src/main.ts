1993ed1706e9e11a5f38f8012fade2281ab1d908
ddhao <603416005@qq.com>
2025-04-02 17:31:21 +0800

    安装api文档 swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
+// 引入swagger api文档
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 // 日志与异常处理
 const logger = WinstonModule.createLogger({
   transports: [new winston.transports.Console()],
@@ -12,6 +15,16 @@ const logger = WinstonModule.createLogger({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger });
+  // 配置api文档
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('Project API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
