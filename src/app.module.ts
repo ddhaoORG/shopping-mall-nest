2429b63e6d188af8327fdaad670aec9c3a1f7c0c
ddhao <603416005@qq.com>
2025-04-02 17:10:47 +0800

    数据库集成Mysql2 + TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      username: process.env.DATABASE_USERNAME, // 账号
+      password: process.env.DATABASE_PASSWORD, // 密码
+      host: process.env.DATABASE_HOST, // host
+      port: Number(process.env.DATABASE_PORT), // 端口
+      database: process.env.DATABASE_NAME, // 库名
+      synchronize: true, // 是否自动将实体类同步到数据库
+      retryDelay: 500, // 重连数据库间隔
+      retryAttempts: 10, // 重连次数
+      autoLoadEntities: true, // 自动加载实体
     }),
   ],
   controllers: [AppController],
